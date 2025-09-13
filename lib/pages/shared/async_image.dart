72b36a8b77dac0353286a626bc3909b0b0d526dd
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2025-09-13 16:15:37 +0700

    fix image thumbnail contentMode

--- lib/pages/shared/async_image.dart
+++ lib/pages/shared/async_image.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 
 class AsyncImage extends StatelessWidget {
-  String url;
+  final String url;
 
-  AsyncImage({super.key, required this.url});
+  const AsyncImage({super.key, required this.url});
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      url,
-      fit: BoxFit.cover,
-      loadingBuilder: (context, child, event) {
-        if (event == null) {
-          return child;
-        } else {
-          return Center(
-            child: CircularProgressIndicator(
-              value: event.expectedTotalBytes != null
-                  ? event.cumulativeBytesLoaded / event.expectedTotalBytes!
-                  : null,
-            ),
-          );
-        }
-      },
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(color: Colors.black12),
+        Image.network(
+          url,
+          fit: BoxFit.cover, // 👈 crop center, maintain aspect ratio
+          alignment: Alignment.center,
+          loadingBuilder: (context, child, event) {
+            if (event == null) {
+              return child;
+            } else {
+              return Center(
+                child: CircularProgressIndicator(
+                  value: event.expectedTotalBytes != null
+                      ? event.cumulativeBytesLoaded / event.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            }
+          },
+        ),
+      ],
     );
   }
 }
