3724bbfcd5aa8c82c42f6f4b27ed7ca4a9b1fbd6
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2025-10-09 10:04:47 +0700

    Flutter-47: Dev - make FavoritesViewModel (#52)
    * Flutter-47: Dev - make FavoritesViewModel
    
    * remove unused space
    
    * Creates `FavoritesViewModel`
    
    * Added UI update trigger

---
+++ lib/pages/favorites/favorites_view_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/widgets.dart';
+import 'package:sketra/data/domain/load_favorite_wallpapers_use_case.dart';
+import 'package:sketra/data/domain/wallpaper_entity.dart';
+
+enum FavoritesViewModelViewState { initial, loading, loaded, error, empty }
+
+typedef ViewState = FavoritesViewModelViewState;
+
+final class FavoritesViewModel extends ChangeNotifier {
+  final LoadFavoriteWallpapersUseCase loadFavoriteWallpapersUseCase;
+
+  FavoritesViewModel({required this.loadFavoriteWallpapersUseCase});
+
+  ViewState _viewState = ViewState.initial;
+
+  ViewState get viewState => _viewState;
+
+  List<WallpaperEntity> _wallpapers = [];
+
+  List<WallpaperEntity> get wallpapers => _wallpapers;
+
+  String _errorMessage = "";
+
+  String get errorMessage => _errorMessage;
+
+  Future<void> onLoad() async {
+    _viewState = ViewState.loading;
+    notifyListeners();
+
+    try {
+      _wallpapers = await loadFavoriteWallpapersUseCase.execute();
+      _viewState = _wallpapers.isEmpty ? ViewState.empty : ViewState.loaded;
+    } catch (e) {
+      _errorMessage = e.toString();
+      _viewState = ViewState.error;
+    }
+    notifyListeners();
+  }
+
+  Future<void> onReload() async {
+    await onLoad();
+  }
+}
